bc5c29d90e84327619d6b618b95ef3863b2ea94d
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-28 13:16:26 +0530

    moved server to main function

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from dash.exceptions import PreventUpdate
 
 # Declaring Global variables
 app = dash.Dash(__name__)
-server = app.server
 project_name = None
 
 # Defining My Functions
@@ -395,6 +394,7 @@ def main():
     global app
     app.layout = create_app_ui()
     app.title = project_name
+    server = app.server
     app.run_server() 
   
     print("End of main function")
